d34ef3d59cae4699ffb8214712293f8ba7ef51fa
Rohan Shah <rohanshah01@gmail.com>
2017-12-02 18:33:28 -0500

    Added Coinbase API check currency
    Checked currency, returned values

--- app.js
+++ app.js
@@ -2,7 +2,15 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var azure = require('botbuilder-azure');
+var Client = require('coinbase').Client; //Coinbase dependency
 
+var client = new Client({
+  'apiKey': 'HoOPwjQ5gPxmmrCV',
+  'apiSecret': 'krqOA9EaKWsk9FErOPVBx6YavAlnpGby',
+  'version':'2017-12-02'
+}); //coinbase client creation
+
+currencyCode = 'USD'; //currencyCode for client
 var documentDbOptions = {
     host: 'https://trader-db.documents.azure.com:443/',
     masterKey: 'hwRHpNoNREov4xYITQnuyH1T9sMqIcKIIV9uiuIR94YZb25StqTIZkSltHLIMBJNo9yyFgu3ausa51lNmuAWRA==',
@@ -14,7 +22,7 @@ var documentDbOptions = {
 var docDbClient = new azure.DocumentDbClient(documentDbOptions);
 var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
 
-var LUIS_MODEL_URL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/f5cc72ec-b8f6-486f-8f5c-88c56bf8d113?subscription-key=fe1edaedf937418d891e2482759bad8e&verbose=true&timezoneOffset=-300";
+var LUIS_MODEL_URL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6da5d499-5a63-4100-a229-d1d31cc46648?subscription-key=fe1edaedf937418d891e2482759bad8e&verbose=true&timezoneOffset=0";
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -38,17 +46,43 @@ var bot = new builder.UniversalBot(connector, function (session) {
 server.post('/api/messages', connector.listen());
 
 //Recognizer
-
+var currency = "";
+var currencySymbol = "";
+var id = "";
 var recognizer = new builder.LuisRecognizer(LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
-bot.dialog('getPrice', function(session){ //Get Price of certain stock
-  
+bot.dialog('getPrice', function(session, args){ //Get Price of certain stock
+	var intent = args.intent;
+	console.log(args);
+	//console.log("\n" + intent.entities);
+	currency = ((builder.EntityRecognizer.findEntity(intent.entities, 'Cryptocurrency')).entity).toLowerCase();
+	console.log(currency);
+	if(currency == "bitcoin"){
+
+		currencySymbol = "BTC";
+	}
+
+	else if(currency == "ethereum"){
+
+		currencySymbol = "ETH";
+	}
+
+	else if(currency == "litecoin"){
+
+		currencySymbol = "LTC";
+	}
+	  client.getBuyPrice({'currencyPair': (currencySymbol + '-' + currencyCode)}, function(err, price) {
+	  session.send( "The price of " + currency + " is " +  JSON.stringify(price.data.amount) + " dollars.");
+	});
 }).triggerAction({ matches: 'getPrice'});
 
 
-bot.dialog('buyCrypto', function(session){//Get User dividends
-
+bot.dialog('buyCrypto', function(session){//Buy crypto
+	client.getPaymentMethods(function(err, paymentMethods) {
+  		console.log(paymentMethods);
+});
+	session.send("We aer here");
 }).triggerAction({ matches: 'buyCrypto'});
 
 
